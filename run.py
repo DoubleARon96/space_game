2db92d1c20a5c1e50300b7ee8eb6ad2c961297e0
DoubleARon96 <>
2023-12-18 19:09:54 +0000

    Feat:sprite class added

--- run.py
+++ run.py
@@ -1,18 +1,31 @@
-#Space wars
+# Space wars
 import os
 import random
 
-#import the Turtle module
+# import the Turtle module
 import turtle
-#required by macos to show window
+# required by macos to show window
 turtle.fd(0)
-#sets animation speed
+# sets animation speed
 turtle.speed(0)
-#change background colour
+# change background colour
 turtle.bgcolor("black")
-#this hides the defult turtle
+# this hides the defult turtle
 turtle.ht()
-#this save memory 
+# this save memory 
 turtle.setundobuffer(1)
 # this speeds up the drawing
 turtle.tracer(1)
+
+class Sprite (turtle.Turtle):
+    """
+    Class set sprite for the base of the objects
+    """
+    def __init__(self, spriteshape, color, startx, starty):
+        turtle.Turtle.__init__ (self, shape = spriteshape)
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.fd(0)
+        self.goto(startx,starty)
+        self.speed = 1
